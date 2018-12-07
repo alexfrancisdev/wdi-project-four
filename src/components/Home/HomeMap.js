f42d9f5f3c3cdd7bfee712fb9be744c09d073d73
AlexFrancis95 <alexfrancis@email.com>
2018-12-07 13:25:00 +0000

    more seeds, and fixes to the backend

--- src/components/Home/HomeMap.js
+++ src/components/Home/HomeMap.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import { Link } from 'react-router-dom';
+import { redMarker, userMarker } from '../../lib/mapIcons';
 
 
 const HomeMap = ({ userPosition, buildings }) => {
@@ -11,17 +12,18 @@ const HomeMap = ({ userPosition, buildings }) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        {userPosition && <Marker position={userPosition}>
+        {userPosition && <Marker icon={ userMarker } position={userPosition}>
           <Popup>
         Your location!
           </Popup>
         </Marker>}
         {buildings && buildings.map(building =>
-          <Marker key={building._id} position={[building.location.lat, building.location.lng]}>
+          <Marker icon={ redMarker } key={building._id} position={[building.location.lat, building.location.lng]}>
             <Popup>
               <Link to={`/buildings/${building._id}`}>
                 <h2>{building.name}</h2>
                 <img src={building.icon}/>
+                <p>test</p>
               </Link>
             </Popup>
           </Marker>
---
+++ src/lib/mapIcons.js
@@ -0,0 +1,63 @@
+import L from 'leaflet';
+
+const redMarker = new L.Icon({
+  iconUrl: require('../assets/icons/icon-red.png'),
+  iconRetinaUrl: require('../assets/icons/icon-red.png'),
+  iconAnchor: null,
+  popupAnchor: [0, -20],
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(25, 41),
+  className: 'leaflet-marker-icon leaflet-zoom-animated leaflet-interactive'
+});
+
+const greenMarker = new L.Icon({
+  iconUrl: require('../assets/icons/icon-green.png'),
+  iconRetinaUrl: require('../assets/icons/icon-green.png'),
+  iconAnchor: null,
+  popupAnchor: [0, -20],
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(25, 41),
+  className: 'leaflet-marker-icon leaflet-zoom-animated leaflet-interactive'
+});
+
+const pinkMarker = new L.Icon({
+  iconUrl: require('../assets/icons/icon-green.png'),
+  iconRetinaUrl: require('../assets/icons/icon-green.png'),
+  iconAnchor: null,
+  popupAnchor: [0, -20],
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(25, 41),
+  className: 'leaflet-marker-icon leaflet-zoom-animated leaflet-interactive'
+});
+
+const purpleMarker = new L.Icon({
+  iconUrl: require('../assets/icons/icon-green.png'),
+  iconRetinaUrl: require('../assets/icons/icon-green.png'),
+  iconAnchor: null,
+  popupAnchor: [0, -20],
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(25, 41),
+  className: 'leaflet-marker-icon leaflet-zoom-animated leaflet-interactive'
+});
+
+const userMarker = new L.Icon({
+  iconUrl: require('../assets/icons/user-icon.png'),
+  iconRetinaUrl: require('../assets/icons/user-icon.png'),
+  iconAnchor: null,
+  popupAnchor: [0, -5],
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(20, 20),
+  className: 'leaflet-marker-icon leaflet-zoom-animated leaflet-interactive'
+});
+
+export { redMarker, greenMarker, pinkMarker, purpleMarker, userMarker };
