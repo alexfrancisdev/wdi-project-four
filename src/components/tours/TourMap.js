b7ca89c5ba9b6414eb9855645b0b62df0c5ea185
AlexFrancis95 <alexfrancis@email.com>
2018-12-10 20:02:35 +0000

    tour show displays map

--- src/components/tours/TourMap.js
+++ src/components/tours/TourMap.js
@@ -3,15 +3,13 @@ import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import { Link } from 'react-router-dom';
 import { redMarker, userMarker } from '../../lib/mapIcons';
 
-const HomeMap = ({ userPosition, buildings }) => {
+const TourMap = ({ userPosition, buildings }) => {
   return (
     <div id='map'>
       <Map center={userPosition} zoom={14}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          // url='https://api.mapbox.com/styles/v1/sophiabarclay/cjphi90qw33w22spcccsrbfzd.html?fresh=true&title=true&access_token=pk.eyJ1Ijoic29waGlhYmFyY2xheSIsImEiOiJjanA5c3V5aGUwMnI3M3JwN2NxY3V6aThqIn0.oMlcrlmaCpK_7H8D0jLOng#10.0/42.362400/-71.020000/0'
-          // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributor'
         />
         {userPosition && <Marker icon={ userMarker } position={userPosition}>
           <Popup>
@@ -24,7 +22,6 @@ const HomeMap = ({ userPosition, buildings }) => {
               <Link to={`/explore/${building._id}`}>
                 <h2>{building.name}</h2>
                 <img src={building.icon}/>
-                <p>test</p>
               </Link>
             </Popup>
           </Marker>
@@ -34,4 +31,4 @@ const HomeMap = ({ userPosition, buildings }) => {
   );
 };
 
-export default HomeMap;
+export default TourMap;
