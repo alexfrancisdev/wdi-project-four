f8e48626520a95446f1141bed3c64005f2029274
Sophia Barclay <barclay.sophia@gmail.com>
2018-12-11 13:07:07 +0000

    homepage checkboxes allbuildings working

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { tokenUserId } from '../../lib/auth';
-
+// import { tokenUserId } from '../../lib/auth';
 import HomeMap from './HomeMap';
 
 class Home extends React.Component {
@@ -9,13 +8,12 @@ class Home extends React.Component {
     super(props);
     this.state = {
       userPosition: null,
-      allBuildings: true,
-      myBuildings: true
+      allBuildingsStatus: true
     };
     this.getLocation = this.getLocation.bind(this);
     this.getBuildings = this.getBuildings.bind(this);
-    this.getUser = this.getUser.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
+    // this.getUser = this.getUser.bind(this);
+    this.handleButtonToggle = this.handleButtonToggle.bind(this);
   }
 
   getLocation(pos) {
@@ -26,70 +24,62 @@ class Home extends React.Component {
 
   getBuildings() {
     axios.get('/api/buildings')
-      .then(res => this.setState({ buildings: res.data }, () => {
+      .then(result => this.setState({ buildings: result.data, filteredBuildings: result.data }, () => {
         console.log('this is state, ', this.state);
-        this.getUser();
+        // this.getUser();
       }));
   }
 
   // Tuesday pick up from here
-  getUser() {
-    const currentUserId = tokenUserId();
-    console.log('currentUserId', currentUserId);
-    axios.get(`/api/users/${currentUserId}`)
-      .then(res => this.setState({ user: res.data }), () => console.log('this.state', this.state));
-  }
+  // getUser() {
+  //   const currentUserId = tokenUserId();
+  //   console.log('currentUserId', currentUserId);
+  //   axios.get(`/api/users/${currentUserId}`)
+  //     .then(res => this.setState({ user: res.data }), () => console.log('this.state', this.state));
+  // }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-    console.log('toggled');
+  handleButtonToggle() {
+    let filteredBuildings = [];
+    if(!this.state.allBuildingsStatus) {
+      filteredBuildings =  this.state.buildings;
+    }
+    this.setState({ allBuildingsStatus: !this.state.allBuildingsStatus, filteredBuildings: filteredBuildings });
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(this.getLocation, this.getBuildings, this.getUser);
+    navigator.geolocation.getCurrentPosition(this.getLocation, this.getBuildings);
+    // navigator.geolocation.getCurrentPosition(this.getLocation, this.getBuildings, this.getUser);
   }
 
   render() {
     return (
       <section className="map-container">
-        <div className="home-buttons">
+        <div className="home-buttons centered-container ">
 
           <form>
-            <span><label >
+            <label >
               <input
+                className="home-button-input"
                 name="allBuildings"
                 type="checkbox"
-                checked={this.state.allBuildings}
-                onChange={this.handleInputChange}
+                checked={this.state.allBuildingsStatus}
+                value="allBuildingsStatus"
+                onChange={this.handleButtonToggle}
               />
               <span className="is-size-5-mobile">All</span>
-            </label></span>
-            <span><label>
-              <input
-                name="myBuildings"
-                type="checkbox"
-                checked={this.state.myBuildings}
-                onChange={this.handleInputChange}
-              />
-              <span className="is-size-5-mobile">My</span>
-            </label></span>
+            </label>
           </form>
 
 
         </div>
         <div className="box-container">
-          {!this.state.userPosition && !this.state.buildings
+          {!this.state.userPosition
             ?
             <p className="is-size-6-mobile centered-container">Loading map...</p>
             :
             <HomeMap
               userPosition={this.state.userPosition}
-              buildings={this.state.buildings}
+              buildings={this.state.filteredBuildings}
             />
           }
         </div>
