4a66675774ae105cac22a2adb93a27917b49b0f1
AlexFrancis95 <alexfrancis@email.com>
2018-12-14 13:21:12 +0000

    Uppercase PORT

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/wdi-project-four';
 const secret = process.env.SECRET || 'hush';
 
